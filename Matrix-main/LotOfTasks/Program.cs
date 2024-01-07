45a4cf2d5fcd0fefe692179a538518f32dbecbd6
CoffeeRaccon <99398267+CoffeeRaccon@users.noreply.github.com>
2024-01-07 15:25:06 +0100

    Add files via upload

---
+++ Matrix-main/LotOfTasks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LotOfTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix1 = new Matrix(10, 3);
+            Matrix matrix2 = new Matrix(3, 10);
+
+            matrix1.SetRandomContent();
+            matrix2.SetRandomContent();
+
+            matrix1.ShowMatrix();
+            Console.WriteLine();
+            matrix2.ShowMatrix();
+
+            Console.WriteLine("---------------------------------");
+            Matrix m3 = matrix1 * matrix2;
+            Console.WriteLine(m3);
+        }
+    }
+}
